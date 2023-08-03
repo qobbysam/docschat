2c5a0caa2594b97c27151c9f762d0fc0361b8dab
samuel abasah <kobysam13@gmail.com>
2023-08-03 18:52:03 -0400

    changed docker to use entrypoint

--- start.sh
+++ start.sh
@@ -19,5 +19,5 @@ celery -A llmapp worker -l info &
 # celery -A your_project_name beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 # Start Gunicorn server with the appropriate number of workers
-gunicorn llmapp.wsgi:application --bind 0.0.0.0:8000 --workers 4 -k uvicorn.workers.UvicornWorker
+gunicorn llmapp.asgi:application --bind 0.0.0.0:8000 --workers 4 -k uvicorn.workers.UvicornWorker
 
