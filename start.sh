e18a7cd9814d00140daa47a73989b527c520a0d6
samuel abasah <kobysam13@gmail.com>
2023-08-03 19:37:04 -0400

    fix allowed host

--- start.sh
+++ start.sh
@@ -16,11 +16,11 @@ python manage.py migrate
 python manage.py collectstatic --noinput
 
 # Start Celery worker
-celery -A llmapp worker -l info &
+celery -A llmapp worker -l info --concurency=3  &
 
 # Start Celery beat (if needed)
 # celery -A your_project_name beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 # Start Gunicorn server with the appropriate number of workers
-gunicorn llmapp.asgi:application --bind 0.0.0.0:8000 --workers 4 -k uvicorn.workers.UvicornWorker
+gunicorn llmapp.asgi:application --bind 0.0.0.0:8000 --workers 2 -k uvicorn.workers.UvicornWorker
 
