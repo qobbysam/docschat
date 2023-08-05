84e0fdd19d3183bedb960212592bf09547722c67
samuel abasah <kobysam13@gmail.com>
2023-08-05 02:08:52 -0400

    seperated celery

--- start_celery.sh
+++ start_celery.sh
@@ -12,11 +12,9 @@ echo $POSTGRESPORT
 # Apply database migrations
 python manage.py migrate
 
-# Collect static files
-python manage.py collectstatic --noinput
 
 # Start Celery worker
-celery -A llmapp worker -l DEBUG  
+celery -A llmapp worker -l DEBUG  --concurrency 6
 
 
 
